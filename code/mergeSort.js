51fec51bb8340cbd6f445784da1c2ce9a173bf0b
RATSIMBAZAFY Anjaraniaina <hei.anjaraniaina@gmail.com>
2022-03-18 23:28:36 +0300

    fix: debugged the code

--- code/mergeSort.js
+++ code/mergeSort.js
@@ -1,4 +1,5 @@
 // This is the main function
+let array = [4,2,5,3,0,9,1,7]
 function mergeSort(an_array){ 
     if(an_array.length <= 1){       // return an array of length == 1 
          return an_array;
@@ -6,31 +7,32 @@ function mergeSort(an_array){
 
     // Divide an_array into two new array 
     let array_right = an_array.splice(an_array.length/2);
-    let array_left = an_array.splice(0, an_array.length/2);
-    
+    let array_left = an_array;  
+
     // Merge two array
     return merge(
         mergeSort(array_left), mergeSort(array_right)
     )
 }
 
-//  Sort arrays 
+//  a function to sort
 let merge = function (array_left, array_right) {
     let resultArray = [];
     let leftIndex = 0;
     let rightIndex = 0;
   
-    // To get a sorted array
+    // This loop sort two array
     while (leftIndex < array_left.length && rightIndex < array_right.length) {
       if (array_left[leftIndex] < array_right[rightIndex]) {
         resultArray.push(array_left[leftIndex]);
         leftIndex++;
+
       } else {
         resultArray.push(array_right[rightIndex]);
         rightIndex++;
       }
     }
 
-    // concatenate
+    // the sorted array
     return resultArray.concat(array_left.slice(leftIndex)).concat(array_right.slice(rightIndex));
   }
